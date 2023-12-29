02b4aee2b69749e5763d8b012d1c9b4d72eec18d
Salim Mouttaki <MOU@inokscapital.com>
2023-12-29 15:06:24 +0100

    push base

---
+++ LinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿class Node
+{
+    Node next = null;
+    int data; 
+
+    public Node(int data)
+    {
+        this.data = data;
+    }
+
+    void appendToTail(int data)
+    {
+        Node end = new Node(data);
+
+        Node node = this;
+
+        while(node.next != null)
+        {
+            node = node.next;
+        }
+        node.next = end;
+    }
+}
