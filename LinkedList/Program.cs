77ef1c739a4f7f91193f2e8dcdcdc19ed88ddd8f
Salim Mouttaki <MOU@inokscapital.com>
2024-01-02 09:35:41 +0100

    added method removeDuplicate and print the list

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -10,7 +10,7 @@ class Noeud
         this.value = data;
     }
 
-    void AjouterALaQueue(int data)
+    void AddToQueue(int data)
     {
         Noeud fin = new Noeud(data);
 
@@ -22,8 +22,48 @@ class Noeud
         }
         noeud.suivant = fin;
     }
+
+    void RemoveDuplicate()
+    {
+        Noeud ptr1 = null,
+             ptr2 = null, dup = null;
+        ptr1 = suivant;
+
+        // Prendre chque éléments 1 par 1 
+        while (ptr1 != null &&
+               ptr1.suivant != null)
+        {
+            ptr2 = ptr1;
+
+            /* Comparer l'élément avec le reste */
+            while (ptr2.suivant != null)
+            {
+                // Si pareil enlever 
+                if (ptr1.value == ptr2.suivant.value)
+                {
+                    dup = ptr2.suivant;
+                    ptr2.suivant = ptr2.suivant.suivant;
+                }
+                else
+                {
+                    ptr2 = ptr2.suivant;
+                }
+            }
+            ptr1 = ptr1.suivant;
+        }
+    }
+
+    void PrintList(Noeud noeud)
+    {
+        while (noeud != null)
+        {
+            Console.Write(noeud.value + " ");
+            noeud = noeud.suivant;
+        }
+    }
 }
 
 //1 Remove Dups
 //Write code to remove duplicates from an unsorted linked list, FOLLOW UP.
 //How would you solve this problem if a temporary buffer is not allowed ?  
+
